616860a15d28c7a9ff77b649f481fcdcfcf55be7
hitzseb <hitzseb@gmail.com>
2023-07-01 18:35:54 -0300

    server url

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,2 +1,2 @@
-// export const URL = "https://hitzseb-wallet-wizard.onrender.com";
-export const URL = "http://localhost:10000";
+export const URL = "https://hitzseb-wallet-wizard.onrender.com";
+// export const URL = "http://localhost:10000";
